7ca90589203392ad0c222f4408586669557228c9
SyntaxPro <107707927+SyntaxPro-lang@users.noreply.github.com>
2026-01-05 05:10:06 -0500

    added BindableEvents
    listen for bindable event for server to server communication

--- server.lua
+++ server.lua
@@ -1,16 +1,19 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RemoteEvent = ReplicatedStorage:WaitForChild("RemoteEvent") or Instance.new('RemoteEvent', ReplicatedStorage)
-
 local ServerNetwork = {}
 
-local listeners = {}
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local RemoteEvent = ReplicatedStorage:FindFirstChild("RemoteEvent") or Instance.new("RemoteEvent", ReplicatedStorage)
+local BindableEvent: BindableEvent = ReplicatedStorage:FindFirstChild("BindableEvent") or Instance.new("BindableEvent", ReplicatedStorage)
+
+local remoteListeners = {}
+local bindListeners = {}
 
 function ServerNetwork:On(messageName: string, callback)
-	listeners[messageName] = callback
+	remoteListeners[messageName] = callback
 end
 
 RemoteEvent.OnServerEvent:Connect(function(player, payload)
-	local handler = listeners[payload.Name]
+	local handler = remoteListeners[payload.Name]
 	if handler then
 		handler(player, payload.Data)
 	end
@@ -25,12 +28,33 @@ function ServerNetwork:Send(target, messageName: string, data: any)
 	if typeof(target) == "Instance" then
 		RemoteEvent:FireClient(target, packet)
 	elseif typeof(target) == "table" then
-		for _, p in target do
-			RemoteEvent:FireClient(p, packet)
+		for _, player in target do
+			RemoteEvent:FireClient(player, packet)
 		end
-	else
-		RemoteEvent:FireAllClients(packet)
 	end
 end
+
+-- listen to bindable event (server to server)
+function ServerNetwork:OnBind(messageName: string, callback)
+	bindListeners[messageName] = callback
+end
+
+function ServerNetwork:FireBind(messageName: string, data: any)
+	BindableEvent:Fire(
+		{
+			Name = messageName,
+			Data = data
+		})
+end
+
+BindableEvent.Event:Connect(function(payload)
+	local name = payload.Name
+	local data = payload.Data
+
+	local callback = bindListeners[name]
+	if callback then
+		callback(data)
+	end
+end)
 
 return ServerNetwork
